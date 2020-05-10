b133bcad67591a3141c09b93a5eee2a4e931af25
Mike Schertl <schert@hm.edu>
2020-05-10 23:26:53 +0200

    move theme to components directory

--- components/theme.js
+++ components/theme.js
@@ -1,7 +1,5 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 import { red } from "@material-ui/core/colors";
-import { blueGrey } from "@material-ui/core/colors";
-import { teal } from "@material-ui/core/colors";
 import { grey } from "@material-ui/core/colors";
 
 // Create a theme instance.
