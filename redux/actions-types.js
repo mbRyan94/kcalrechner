7dcb0f9eff75ecc6eaf9fcb60a180d56ff8c7b23
Mike Schertl <schert@hm.edu>
2020-05-12 00:37:51 +0200

    add redux action types

---
+++ redux/actions-types.js
@@ -0,0 +1,6 @@
+export const ADD_WEIGHT = "ADD_WEIGHT";
+export const ADD_AGE = "ADD_AGE";
+export const ADD_HEIGHT = "ADD_HEIGHT";
+export const ADD_ACTIVITY_LEVEL = "ADD_ACTIVITY_LEVEL";
+export const ADD_GENDER = "ADD_GENDER";
+export const ADD_ACTIVE_KCAL = "ADD_ACTIVE_KCAL";
